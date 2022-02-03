fd99998514e0c3d4d92d8e18b04e2d8f2d5a35e6
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-02-03 08:35:53 -0600

    page counter exercise

---
+++ src/main/java/PageCounterServlet.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "PageCountServlet", urlPatterns = "/count")
+public class PageCounterServlet extends HttpServlet {
+    int count = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        count ++;
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("count " + count);
+    }
+}
