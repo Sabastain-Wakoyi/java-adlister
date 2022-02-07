4326852c3ef97999ee224db5a00afccd2fc7b8e4
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-02-07 12:14:21 -0600

    javabeans lec

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -9,18 +9,63 @@ public class Employee implements Serializable {
     private Date birthDate;
     private Date hireDate;
 
-    public Employee(long empNo) {
+    public Employee() {
+    }
+
+    public Employee(long empNo, String firstName, String lastName, char gender, Date birthDate, Date hireDate) {
+        this.empNo = empNo;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.hireDate = hireDate;
+    }
+
+    public long getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(long empNo) {
         this.empNo = empNo;
     }
 
-    public Employee(String firstName, String lastName) {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public Employee(char gender, Date birthDate, Date hireDate) {
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
         this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
         this.hireDate = hireDate;
     }
 }
